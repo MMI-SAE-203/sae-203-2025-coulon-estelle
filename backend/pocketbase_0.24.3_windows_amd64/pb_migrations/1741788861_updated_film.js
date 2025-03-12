fa90966b0cc7c79fd63cf2c7ebe0b464740907ba
coulon-estelle <estelle.coulon@edu.univ-fcomte.fr>
2025-03-12 15:42:47 +0100

    add new pages and components for SCI-FISTIVAL, update database migrations, and modify existing components

---
+++ backend/pocketbase_0.24.3_windows_amd64/pb_migrations/1741788861_updated_film.js
@@ -0,0 +1,26 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2773080466")
+
+  // add field
+  collection.fields.addAt(9, new Field({
+    "hidden": false,
+    "id": "date2184779177",
+    "max": "",
+    "min": "",
+    "name": "date_de_projection",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "date"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2773080466")
+
+  // remove field
+  collection.fields.removeById("date2184779177")
+
+  return app.save(collection)
+})
